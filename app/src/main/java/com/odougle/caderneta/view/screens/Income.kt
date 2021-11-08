e9c6b933d1c72f3f17d043a838ad28c94711e687
Douglas Gomes <douglasgommez@gmail.com>
2021-11-08 17:41:08 -0300

    add sample to see on each screen

--- app/src/main/java/com/odougle/caderneta/view/screens/Income.kt
+++ app/src/main/java/com/odougle/caderneta/view/screens/Income.kt
@@ -1,8 +1,18 @@
 package com.odougle.caderneta.view.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun IncomeScreen() {
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "RECEITAS")
+    }
 }
