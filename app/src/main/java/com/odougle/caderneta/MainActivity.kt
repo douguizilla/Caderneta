d064f2625f88ab189aefdd48f24c08624e4c07a8
Douglas Gomes <douglasgommez@gmail.com>
2021-11-08 17:30:05 -0300

    call MainScreen at MainActivity.kt

--- app/src/main/java/com/odougle/caderneta/MainActivity.kt
+++ app/src/main/java/com/odougle/caderneta/MainActivity.kt
@@ -9,30 +9,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.odougle.caderneta.ui.theme.CadernetaTheme
+import com.odougle.caderneta.view.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CadernetaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CadernetaTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
