31ef7262e1c0979078191782fcc5798e0d366a1c
Douglas Gomes <douglasgommez@gmail.com>
2021-11-08 16:58:07 -0300

    created BottomBarScreen sealed class

---
+++ app/src/main/java/com/odougle/caderneta/view/navigation/BottomBarScreen.kt
@@ -0,0 +1,47 @@
+package com.odougle.caderneta.view.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+){
+    object Home: BottomBarScreen(
+        route = "home",
+        title = "Início",
+        icon = Icons.Default.List
+    )
+
+    object Income: BottomBarScreen(
+        route = "income",
+        title = "Receitas",
+        icon = Icons.Default.List
+    )
+
+    object NewBill: BottomBarScreen(
+        route = "newBill",
+        title = "Add",
+        icon = Icons.Default.Add
+    )
+
+    object Outlay: BottomBarScreen(
+        route = "outlay",
+        title = "Despesas",
+        icon = Icons.Default.List
+    )
+
+    object Goals: BottomBarScreen(
+        route = "goals",
+        title = "Metas",
+        icon = Icons.Default.Info
+    )
+
+
+
+
+}
