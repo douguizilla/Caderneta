dbaa2bd705083c63560328f52233c636ec8c6b40
Douglas Gomes <douglasgommez@gmail.com>
2021-11-20 17:46:24 -0300

    Created a unique file for TopBar

--- app/src/main/java/com/odougle/caderneta/view/components/TopAppBar.kt
+++ app/src/main/java/com/odougle/caderneta/view/components/TopAppBar.kt
@@ -1,16 +1,66 @@
 package com.odougle.caderneta.view.components
 
-import androidx.compose.material.MaterialTheme
+import ActionItem
+import ActionMenu
+import androidx.annotation.StringRes
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.odougle.caderneta.R
 
 @Composable
-fun <T> TopAppBar(
-    title: @Composable() () -> Unit,
-    actionData: List<T>,
-    color: Color = MaterialTheme.colors.primary,
-    navigationIcon: @Composable() (() -> Unit)? = null,
-    action: @Composable() (T) -> Unit
-) {
+fun CadernetaTopAppBar() {
+    changeStatusBarColor()
+    val items = listOf(
+        ActionItem(
+            R.string.settings,
+            painterResource(id = R.drawable.ic_settings),
+            OverflowMode.ALWAYS_OVERFLOW,
+            doAction = { settingsAction() })
+    )
 
+    TopAppBar(
+        title = {
+            Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.h1, color = Color.DarkGray)
+        },
+        backgroundColor = Color.White,
+        actions = {
+            ActionMenu(items = items)
+        }
+    )
 }
+
+@Composable
+fun changeStatusBarColor(){
+    val systemUiController = rememberSystemUiController()
+    if(isSystemInDarkTheme()){
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    }else{
+        systemUiController.setSystemBarsColor(
+            color = Color.White
+        )
+    }
+}
+
+private fun settingsAction() {
+    TODO("Not yet implemented")
+}
+
+//fun TopAppBar(
+//    title: @Composable () -> Unit,
+//    modifier: Modifier = Modifier,
+//    navigationIcon: @Composable (() -> Unit)? = null,
+//    actions: @Composable RowScope.() -> Unit = {},
+//    backgroundColor: Color = MaterialTheme.colors.primarySurface,
+//    contentColor: Color = contentColorFor(backgroundColor),
+//    elevation: Dp = AppBarDefaults.TopAppBarElevation
+//)
