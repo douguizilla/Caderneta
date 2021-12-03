3dcc893f23edba162a236a13fd070cefb3bd38c9
Douglas Gomes <douglasgommez@gmail.com>
2021-12-03 18:38:18 -0300

    refactoring

--- app/src/main/java/com/odougle/caderneta/view/screens/bottomSheetDialogs/NewIncome.kt
+++ app/src/main/java/com/odougle/caderneta/view/screens/bottomSheetDialogs/NewIncome.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.odougle.caderneta.util.DEFAULT_PADDING
 import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
@@ -14,20 +14,22 @@ import kotlinx.coroutines.launch
 fun NewIncome(
     bottomSheetState: ModalBottomSheetState
 ) {
-    val coroutine = rememberCoroutineScope()
-    Column(modifier = Modifier
-        .background(MaterialTheme.colors.background)
-        .padding(16.dp)
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(DEFAULT_PADDING)
     ) {
-        var descriptionText by remember { mutableStateOf("")}
-        var valueText by remember { mutableStateOf("")}
-        var dateText by remember { mutableStateOf("")}
+        var descriptionText by remember { mutableStateOf("") }
+        var valueText by remember { mutableStateOf("") }
+        var dateText by remember { mutableStateOf("") }
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = descriptionText,
-            onValueChange = {descriptionText = it},
-            label = { Text(text = "Descrição")}
+            onValueChange = { descriptionText = it },
+            label = { Text(text = "Descrição") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -35,8 +37,8 @@ fun NewIncome(
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = valueText,
-            onValueChange = {valueText = it},
-            label = { Text(text = "Valor")}
+            onValueChange = { valueText = it },
+            label = { Text(text = "Valor") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -44,19 +46,20 @@ fun NewIncome(
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = dateText,
-            onValueChange = {dateText = it},
-            label = { Text(text = "Data")}
+            onValueChange = { dateText = it },
+            label = { Text(text = "Data") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Row(modifier = Modifier.fillMaxWidth(),
+        Row(
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Button(
-               modifier = Modifier.weight(1f),
+                modifier = Modifier.weight(1f),
                 onClick = {
-                    coroutine.launch {
+                    coroutineScope.launch {
                         bottomSheetState.hide()
                     }
                 }
@@ -65,6 +68,7 @@ fun NewIncome(
             }
 
             Spacer(modifier = Modifier.width(16.dp))
+
             Button(
                 modifier = Modifier.weight(1f),
                 onClick = { }
@@ -73,8 +77,7 @@ fun NewIncome(
             }
 
         }
-
-
     }
 }
+
 
