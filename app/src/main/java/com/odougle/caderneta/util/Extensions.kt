002d9c455277203fc3b7894a58771b6761623e3f
Douglas Gomes <douglasgommez@gmail.com>
2021-12-03 20:14:14 -0300

    created an extension function to convert string length in dp value

--- app/src/main/java/com/odougle/caderneta/util/Extensions.kt
+++ app/src/main/java/com/odougle/caderneta/util/Extensions.kt
@@ -2,8 +2,18 @@ package com.odougle.caderneta.util
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Int.fromStringRes() : String{
     return stringResource(id = this)
 }
+
+@Composable
+fun String.parseToDP() =
+    if(this.length < 5){
+        (this.length * 13).dp
+    }else{
+        (this.length * 10).dp
+    }
