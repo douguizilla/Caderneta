d03c18a332178c36168734da8e6b43d036abc02c
Douglas Gomes <douglasgommez@gmail.com>
2021-12-03 21:05:13 -0300

    created IncomeItem.kt

---
+++ app/src/main/java/com/odougle/caderneta/view/screens/lazycolumns/items/IncomeItem.kt
@@ -0,0 +1,22 @@
+package com.odougle.caderneta.view.screens.lazycolumns.items
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun IncomeItem(
+    tag: String,
+    description: String,
+    date: String,
+    value: String,
+    color: Color = Color.Green
+) {
+    ItemBase(
+        tag = tag,
+        description = description,
+        date = date,
+        value = value,
+        isValuePositive = true,
+        color = color
+    )
+}
