7862a2ed21bbc3bc9cdc230655736afcc4239734
Douglas Gomes <douglasgommez@gmail.com>
2021-12-04 14:37:19 -0300

    created Outlay.kt and OutlayScreen.kt lazy column

---
+++ app/src/main/java/com/odougle/caderneta/model/Outlay.kt
@@ -0,0 +1,8 @@
+package com.odougle.caderneta.model
+
+data class Outlay(
+    val tag: String,
+    val description: String,
+    val date: String,
+    val value: String,
+)
--- app/src/main/java/com/odougle/caderneta/view/screens/lazycolumns/OutlayScreen.kt
+++ app/src/main/java/com/odougle/caderneta/view/screens/lazycolumns/OutlayScreen.kt
@@ -1,18 +1,50 @@
 package com.odougle.caderneta.view.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.odougle.caderneta.model.Outlay
+import com.odougle.caderneta.view.screens.lazycolumns.items.OutlayItem
 
 @Composable
 fun OutlayScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val outlayList = listOf(
+        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+        Outlay("despesa", "credito", "11/10/2021", "1.000,00"),
+        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
+        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
+        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
+        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
+        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
+        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
+        Outlay("despesa", "famarcia", "10/11/2021", "2.200,00"),
+        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+        Outlay("despesa", "mercado", "07/10/2008", "4.000,00"),
+    )
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text(text = "DESPESAS")
+        items(outlayList) { outlay ->
+            OutlayItem(
+                tag = outlay.tag,
+                description = outlay.description,
+                date = outlay.date,
+                value = outlay.value
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
 }
