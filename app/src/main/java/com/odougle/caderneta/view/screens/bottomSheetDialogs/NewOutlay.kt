01e86c7ae23e2dd72c018815b59dcabb8314e8cb
Douglas Gomes <douglasgommez@gmail.com>
2021-12-07 20:43:20 -0300

    NewOutlay.kt created, but it's temporary

---
+++ app/src/main/java/com/odougle/caderneta/view/screens/bottomSheetDialogs/NewOutlay.kt
@@ -0,0 +1,97 @@
+package com.odougle.caderneta.view.screens.bottomSheetDialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.odougle.caderneta.util.DEFAULT_PADDING
+import kotlinx.coroutines.launch
+
+@ExperimentalMaterialApi
+@Composable
+fun NewOutlay(
+    bottomSheetState: ModalBottomSheetState
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(DEFAULT_PADDING)
+    ) {
+        var tagText by remember { mutableStateOf("") }
+        var descriptionText by remember { mutableStateOf("") }
+        var valueText by remember { mutableStateOf("") }
+        var dateText by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = tagText,
+            onValueChange = { tagText = it },
+            label = { Text(text = "Tag") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = descriptionText,
+            onValueChange = { descriptionText = it },
+            label = { Text(text = "Descrição") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = valueText,
+                onValueChange = { valueText = it },
+                label = { Text(text = "Valor") }
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = dateText,
+                onValueChange = { dateText = it },
+                label = { Text(text = "Data") }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    coroutineScope.launch {
+                        bottomSheetState.hide()
+                    }
+                }
+            ) {
+                Text(text = "CANCELAR")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { }
+            ) {
+                Text(text = "ADICIONAR")
+            }
+
+        }
+    }
+}
