e2e034335ef3bb041376b35845fea8fc5676c0bb
Douglas Gomes <douglasgommez@gmail.com>
2021-12-07 20:39:40 -0300

    Goal.kt data class create

---
+++ app/src/main/java/com/odougle/caderneta/model/Goal.kt
@@ -0,0 +1,12 @@
+package com.odougle.caderneta.model
+
+data class Goal(
+    val tag: String = "meta",
+    val description: String,
+    val total: Double,
+    val quantity: Int,
+    val paid: Int,
+    val billingDate: Int,
+    val creationDate: String,
+    val finishDate: String
+)
