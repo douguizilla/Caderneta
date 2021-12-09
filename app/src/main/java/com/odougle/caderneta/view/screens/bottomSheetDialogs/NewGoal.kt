308334c594dc940ddefe97f51def15d57fdefc57
Douglas Gomes <douglasgommez@gmail.com>
2021-12-09 17:40:50 -0300

    created NewGoal.kt

---
+++ app/src/main/java/com/odougle/caderneta/view/screens/bottomSheetDialogs/NewGoal.kt
@@ -0,0 +1,115 @@
+package com.odougle.caderneta.view.screens.bottomSheetDialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.odougle.caderneta.util.DEFAULT_PADDING
+import kotlinx.coroutines.launch
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun NewGoal(
+ //   bottomSheetState: ModalBottomSheetState
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(DEFAULT_PADDING)
+    ) {
+        val tagText = "meta"
+        var descriptionText by remember { mutableStateOf("") }
+        var valueText by remember { mutableStateOf("") }
+        var dateText by remember { mutableStateOf("") }
+        var billingDate by remember { mutableStateOf("") }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Adicione uma nova meta",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = descriptionText,
+            onValueChange = { descriptionText = it },
+            label = { Text(text = "Descrição") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = valueText,
+                onValueChange = { valueText = it },
+                label = { Text(text = "Valor total") }
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = dateText,
+                onValueChange = { dateText = it },
+                label = { Text(text = "Valor parcela") }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = billingDate,
+            onValueChange = { billingDate = it },
+            label = { Text(text = "Data primeira cobrança") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    coroutineScope.launch {
+//                        bottomSheetState.hide()
+                    }
+                }
+            ) {
+                Text(text = "CANCELAR")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { }
+            ) {
+                Text(text = "ADICIONAR")
+            }
+
+        }
+    }
+}
