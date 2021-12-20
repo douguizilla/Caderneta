a799ddba7580a6cb2ca9c70e96c5a565b6c76941
Douglas Gomes <douglasgommez@gmail.com>
2021-12-20 13:31:23 -0300

    turn Income.kt a Room entity

--- app/src/main/java/com/odougle/caderneta/domain/model/Income.kt
+++ app/src/main/java/com/odougle/caderneta/domain/model/Income.kt
@@ -1,8 +1,13 @@
 package com.odougle.caderneta.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "income")
 data class Income(
     val tag: String,
     val description: String,
     val date: String,
     val value: String,
+    @PrimaryKey val id: Int? = null
 )
