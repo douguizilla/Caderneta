e6438ef9bb5792724caf1323840e3325d5c58ce2
Douglas Gomes <douglasgommez@gmail.com>
2021-12-23 16:23:05 -0300

    Create AddIncomeUseCase.kt

---
+++ app/src/main/java/com/odougle/caderneta/features/domain/use_case/AddIncomeUseCase.kt
@@ -0,0 +1,12 @@
+package com.odougle.caderneta.features.domain.use_case
+
+import com.odougle.caderneta.features.domain.model.Income
+import com.odougle.caderneta.features.domain.repository.DatabaseRepository
+
+class AddIncomeUseCase(
+    private val repository: DatabaseRepository
+) {
+    suspend operator fun invoke(income: Income){
+        repository.insertIncome(income)
+    }
+}
