9130bc866bdbf9bb5b30c193ac8211f71c401a7d
Douglas Gomes <douglasgommez@gmail.com>
2021-12-22 21:14:57 -0300

    create GoalDao.kt

---
+++ app/src/main/java/com/odougle/caderneta/features/data/data_source/GoalDao.kt
@@ -0,0 +1,20 @@
+package com.odougle.caderneta.features.data.data_source
+
+import androidx.room.*
+import com.odougle.caderneta.features.domain.model.Goal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GoalDao {
+    @Query("SELECT * FROM goal")
+    fun getGoals(): Flow<List<Goal>>
+
+    @Query("SELECT * FROM goal WHERE id = :id")
+    suspend fun getGoalById(id: Int) : Goal?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGoal(note: Goal)
+
+    @Delete
+    suspend fun deleteGoal(note: Goal)
+}
