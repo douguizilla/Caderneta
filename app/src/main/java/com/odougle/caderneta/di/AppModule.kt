9cec8eb800da8161502f2c4fecaff080d99e45c0
Douglas Gomes <douglasgommez@gmail.com>
2022-01-04 21:17:12 -0300

    Create functions for providing the repository and the use cases functions

--- app/src/main/java/com/odougle/caderneta/di/AppModule.kt
+++ app/src/main/java/com/odougle/caderneta/di/AppModule.kt
@@ -3,6 +3,21 @@ package com.odougle.caderneta.di
 import android.app.Application
 import androidx.room.Room
 import com.odougle.caderneta.features.data.data_source.Database
+import com.odougle.caderneta.features.data.repository.DatabaseRepositoryImpl
+import com.odougle.caderneta.features.domain.repository.DatabaseRepository
+import com.odougle.caderneta.features.domain.use_case.CadernetaUseCases
+import com.odougle.caderneta.features.domain.use_case.goal.AddGoalUseCase
+import com.odougle.caderneta.features.domain.use_case.goal.DeleteGoalUseCase
+import com.odougle.caderneta.features.domain.use_case.goal.GetGoalUseCase
+import com.odougle.caderneta.features.domain.use_case.goal.GetGoalsUseCase
+import com.odougle.caderneta.features.domain.use_case.income.AddIncomeUseCase
+import com.odougle.caderneta.features.domain.use_case.income.DeleteIncomeUseCase
+import com.odougle.caderneta.features.domain.use_case.income.GetIncomeUseCase
+import com.odougle.caderneta.features.domain.use_case.income.GetIncomesUseCase
+import com.odougle.caderneta.features.domain.use_case.outlay.AddOutlayUseCase
+import com.odougle.caderneta.features.domain.use_case.outlay.DeleteOutlayUseCase
+import com.odougle.caderneta.features.domain.use_case.outlay.GetOutlayUseCase
+import com.odougle.caderneta.features.domain.use_case.outlay.GetOutlaysUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,11 +30,36 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(app: Application) : Database{
+    fun provideDatabase(app: Application): Database {
         return Room.databaseBuilder(
             app,
             Database::class.java,
             Database.DATABASE_NAME
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabaseRepository(db: Database): DatabaseRepository {
+        return DatabaseRepositoryImpl(db.databaseDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCadernetaUseCases(repository: DatabaseRepository): CadernetaUseCases {
+        return CadernetaUseCases(
+            addGoalUseCase = AddGoalUseCase(repository),
+            addIncomeUseCase = AddIncomeUseCase(repository),
+            addOutlayUseCase = AddOutlayUseCase(repository),
+            deleteGoalUseCase = DeleteGoalUseCase(repository),
+            deleteIncomeUseCase = DeleteIncomeUseCase(repository),
+            deleteOutlayUseCase = DeleteOutlayUseCase(repository),
+            getGoalUseCase = GetGoalUseCase(repository),
+            getGoalsUseCase = GetGoalsUseCase(repository),
+            getIncomeUseCase = GetIncomeUseCase(repository),
+            getIncomesUseCase = GetIncomesUseCase(repository),
+            getOutlayUseCase = GetOutlayUseCase(repository),
+            getOutlaysUseCase = GetOutlaysUseCase(repository)
+        )
     }
 }
