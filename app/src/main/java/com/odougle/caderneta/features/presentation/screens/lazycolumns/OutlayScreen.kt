0b2121ef6e386facaf0a32a46ed0090209b58f6e
Douglas Gomes <douglasgommez@gmail.com>
2022-01-19 19:04:51 -0300

    change data source on OutlayScreen.kt

--- app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/OutlayScreen.kt
+++ app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/OutlayScreen.kt
@@ -9,28 +9,34 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.odougle.caderneta.features.domain.model.Outlay
+import com.odougle.caderneta.features.presentation.screens.CadernetaViewModel
 import com.odougle.caderneta.features.presentation.screens.lazycolumns.items.OutlayItem
 
 @Composable
-fun OutlayScreen() {
-    val outlayList = listOf(
-        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-        Outlay("despesa", "credito", "11/10/2021", "1.000,00"),
-        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
-        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
-        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
-        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
-        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
-        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
-        Outlay("despesa", "famarcia", "10/11/2021", "2.200,00"),
-        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-        Outlay("despesa", "mercado", "07/10/2008", "4.000,00"),
-    )
+fun OutlayScreen(
+    viewModel: CadernetaViewModel = hiltViewModel()
+) {
+    val outlayList = viewModel.outlays.value
+//    val outlayList = listOf(
+//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+//        Outlay("despesa", "credito", "11/10/2021", "1.000,00"),
+//        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
+//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+//        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
+//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+//        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
+//        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
+//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+//        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
+//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
+//        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
+//        Outlay("despesa", "famarcia", "10/11/2021", "2.200,00"),
+//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
+//        Outlay("despesa", "mercado", "07/10/2008", "4.000,00"),
+//    )
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
