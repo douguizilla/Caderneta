9a96d544c21513c9d078ae901eb85a4e4ad62724
Douglas Gomes <douglasgommez@gmail.com>
2022-01-22 19:33:05 -0300

    import optmization

--- app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/IncomeScreen.kt
+++ app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/IncomeScreen.kt
@@ -2,9 +2,7 @@ package com.odougle.caderneta.view.screens
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
