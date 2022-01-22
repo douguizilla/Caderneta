8daf5a339f7dea90bc6fb71abe622a7fa71e019d
Douglas Gomes <douglasgommez@gmail.com>
2022-01-22 19:51:10 -0300

    implemet delete swipe action for Goals and Outlay lists

--- app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/GoalsScreen.kt
+++ app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/GoalsScreen.kt
@@ -1,47 +1,113 @@
 package com.odougle.caderneta.view.screens
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.odougle.caderneta.features.domain.model.Goal
 import com.odougle.caderneta.features.presentation.screens.CadernetaViewModel
 import com.odougle.caderneta.features.presentation.screens.lazycolumns.items.GoalItem
+import com.odougle.caderneta.features.presentation.util.ALL_SIDES_ROUNDED_CORNER_SHAPE
 
+@ExperimentalMaterialApi
 @Composable
 fun GoalsScreen(
     viewModel: CadernetaViewModel = hiltViewModel()
 ) {
     val goalsList = viewModel.goals.value
-//    val goalsList = listOf(
-//        Goal("Viagem", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Carro", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Dentista", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Economia", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Reserva", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Consórcio", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Viagem", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Viagem", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022"),
-//        Goal("Viagem", 1000.0, 100.0, 10,2, 10, "10/11/2021", "10/01/2022")
-//    )
-    LazyColumn(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) {
-        items(goalsList) { goal ->
-            GoalItem(
-                description = goal.description,
-                total = goal.total,
-                portion = goal.portion,
-                quantity = goal.quantity,
-                paid = goal.paid,
-                billingDate = goal.billingDate,
-                creationDate = goal.creationDate,
-                finishDate = goal.finishDate
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+    if(goalsList.isEmpty()){
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Você ainda não possui nenhuma meta? Sério?")
+        }
+    }else{
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+
+            items(goalsList, { goal: Goal -> goal.id }) { goal ->
+                val dismissState = rememberDismissState()
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    viewModel.deleteGoal(goal)
+                }
+                SwipeToDismiss(
+                    state = dismissState,
+                    modifier = Modifier
+                        .padding(vertical = Dp(1f)),
+                    directions = setOf(
+                        DismissDirection.EndToStart
+                    ),
+                    dismissThresholds = { direction ->
+                        FractionalThreshold(if (direction == DismissDirection.EndToStart) 0.1f else 0.05f)
+                    },
+                    background = {
+                        Card(shape = ALL_SIDES_ROUNDED_CORNER_SHAPE){
+                            val color by animateColorAsState(
+                                when (dismissState.targetValue) {
+                                    DismissValue.Default -> Color.White
+                                    else -> Color.Red
+                                }
+                            )
+                            val alignment = Alignment.CenterEnd
+                            val icon = Icons.Default.Delete
+
+                            val scale by animateFloatAsState(
+                                if (dismissState.targetValue == DismissValue.Default) 0.75f else 1f
+                            )
+
+                            Box(
+                                Modifier
+                                    .fillMaxSize()
+                                    .background(color)
+                                    .padding(horizontal = Dp(20f)),
+                                contentAlignment = alignment
+                            ) {
+                                Icon(
+                                    icon,
+                                    contentDescription = "Delete Icon",
+                                    modifier = Modifier.scale(scale),
+                                    tint = Color.White
+                                )
+                            }
+                        }
+                    },
+                    dismissContent = {
+                        Card(shape = ALL_SIDES_ROUNDED_CORNER_SHAPE){
+                            GoalItem(
+                                description = goal.description,
+                                total = goal.total,
+                                portion = goal.portion,
+                                quantity = goal.quantity,
+                                paid = goal.paid,
+                                billingDate = goal.billingDate,
+                                creationDate = goal.creationDate,
+                                finishDate = goal.finishDate
+                            )
+                        }
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
 }
--- app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/OutlayScreen.kt
+++ app/src/main/java/com/odougle/caderneta/features/presentation/screens/lazycolumns/OutlayScreen.kt
@@ -1,56 +1,109 @@
 package com.odougle.caderneta.view.screens
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.odougle.caderneta.features.domain.model.Outlay
 import com.odougle.caderneta.features.presentation.screens.CadernetaViewModel
 import com.odougle.caderneta.features.presentation.screens.lazycolumns.items.OutlayItem
+import com.odougle.caderneta.features.presentation.util.ALL_SIDES_ROUNDED_CORNER_SHAPE
 
+@ExperimentalMaterialApi
 @Composable
 fun OutlayScreen(
     viewModel: CadernetaViewModel = hiltViewModel()
 ) {
     val outlayList = viewModel.outlays.value
-//    val outlayList = listOf(
-//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-//        Outlay("despesa", "credito", "11/10/2021", "1.000,00"),
-//        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
-//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-//        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
-//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-//        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
-//        Outlay("despesa", "bico", "10/11/2021", "2.200,00"),
-//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-//        Outlay("despesa", "famarcia", "07/10/2008", "4.000,00"),
-//        Outlay("despesa", "famarcia", "10/10/2021", "2.000,00"),
-//        Outlay("despesa", "freela", "11/10/2021", "1.000,00"),
-//        Outlay("despesa", "famarcia", "10/11/2021", "2.200,00"),
-//        Outlay("despesa", "investimento", "09/10/2021", "5.000,00"),
-//        Outlay("despesa", "mercado", "07/10/2008", "4.000,00"),
-//    )
+    if(outlayList.isEmpty()){
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Você ainda não possui despesa, isso é ótimo!")
+        }
+    }else{
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        items(outlayList) { outlay ->
-            OutlayItem(
-                tag = outlay.tag,
-                description = outlay.description,
-                date = outlay.date,
-                value = outlay.value
-            )
+            items(outlayList, { outlay: Outlay -> outlay.id }) { outlay ->
+                val dismissState = rememberDismissState()
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    viewModel.deleteOutlay(outlay)
+                }
+                SwipeToDismiss(
+                    state = dismissState,
+                    modifier = Modifier
+                        .padding(vertical = Dp(1f)),
+                    directions = setOf(
+                        DismissDirection.EndToStart
+                    ),
+                    dismissThresholds = { direction ->
+                        FractionalThreshold(if (direction == DismissDirection.EndToStart) 0.1f else 0.05f)
+                    },
+                    background = {
+                        Card(shape = ALL_SIDES_ROUNDED_CORNER_SHAPE){
+                            val color by animateColorAsState(
+                                when (dismissState.targetValue) {
+                                    DismissValue.Default -> Color.White
+                                    else -> Color.Red
+                                }
+                            )
+                            val alignment = Alignment.CenterEnd
+                            val icon = Icons.Default.Delete
 
-            Spacer(modifier = Modifier.height(8.dp))
+                            val scale by animateFloatAsState(
+                                if (dismissState.targetValue == DismissValue.Default) 0.75f else 1f
+                            )
+
+                            Box(
+                                Modifier
+                                    .fillMaxSize()
+                                    .background(color)
+                                    .padding(horizontal = Dp(20f)),
+                                contentAlignment = alignment
+                            ) {
+                                Icon(
+                                    icon,
+                                    contentDescription = "Delete Icon",
+                                    modifier = Modifier.scale(scale),
+                                    tint = Color.White
+                                )
+                            }
+                        }
+                    },
+                    dismissContent = {
+                        Card(shape = ALL_SIDES_ROUNDED_CORNER_SHAPE){
+                            OutlayItem(
+                                tag = outlay.tag,
+                                description = outlay.description,
+                                date = outlay.date,
+                                value = outlay.value
+                            )
+                        }
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
 }
